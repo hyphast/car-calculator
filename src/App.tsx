29a8fe10fd87404abdbfa42a662aef8f66299ca5
hyphast <oshcherbakov2018@mail.ru>
2023-02-14 00:32:35 +0400

    🎉 Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app__container">
+      <h2>Рассчитайте стоимость автомоля в лизинг</h2>
+    </div>
+  )
+}
+
+export default App
